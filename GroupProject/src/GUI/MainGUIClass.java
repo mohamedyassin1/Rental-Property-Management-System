e15de3682fb65a2b7782c88dd6fc1e6c5912b28c
Mohamed Yassin <73013959+mohamedyassin1@users.noreply.github.com>
2021-12-04 00:13:17 -0700

    First Commit

--- GroupProject/src/GUI/MainGUIClass.java
+++ GroupProject/src/GUI/MainGUIClass.java
@@ -4,6 +4,8 @@ public class MainGUIClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		//I am just checking if Git Works
+		System.out.println("yo");
 		Component mainMenu = new MainMenu();
 	}
 
