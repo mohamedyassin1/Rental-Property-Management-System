7e21c37b5367fa6a8cb75f4c0b24cee6d956e34e
Mohamed Yassin <73013959+mohamedyassin1@users.noreply.github.com>
2021-12-04 00:13:46 -0700

    Update MainGUIClass.java

--- GroupProject/src/GUI/MainGUIClass.java
+++ GroupProject/src/GUI/MainGUIClass.java
@@ -5,6 +5,7 @@ public class MainGUIClass {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		//I am just checking if Git Works
+		//Ok it works, now i will commit everything to my branch and only merge if it works perfectly
 		System.out.println("yo");
 		Component mainMenu = new MainMenu();
 	}
