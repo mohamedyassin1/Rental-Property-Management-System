6dbfbf636cc673773451aff02cd15f439c9c4f2c
Ahmed Waly <67707288+ahmedw14@users.noreply.github.com>
2021-12-08 11:09:45 -0700

    Update Manager.java

--- GroupProject/src/model/Manager.java
+++ GroupProject/src/model/Manager.java
@@ -1,12 +1,10 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Manager extends User{
     public Manager(String email, String name, String pass){
         super(email, name, pass);
     }
-    public void createReport(int numHousesRented, int numHousesListed, int numActiveListings, ArrayList<Property> housesRented){
+    public void createReport(int numHousesRented, int numHousesListed, int numActiveListings, String[][] housesRented){
         Report createReport = new Report(numHousesRented, numHousesListed, numActiveListings, housesRented);
         createReport.generate();
     }
