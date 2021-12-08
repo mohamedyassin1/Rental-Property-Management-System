2af0760e38063002f9510accb2c1dc1353f4a824
Ahmed Waly <67707288+ahmedw14@users.noreply.github.com>
2021-12-08 11:09:05 -0700

    Update Report.java

--- GroupProject/src/model/Report.java
+++ GroupProject/src/model/Report.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.util.ArrayList;
 import java.io.*;
 
 //Finished
@@ -8,8 +7,8 @@ public class Report {
     private int numHousesRented;
     private int numHousesListed;
     private int numActiveListings;
-    private ArrayList<Property> housesRented;
-    public Report(int numHousesRented, int numHousesListed, int numActiveListings, ArrayList<Property> housesRented) {
+    private String[][] housesRented;
+    public Report(int numHousesRented, int numHousesListed, int numActiveListings,String[][] housesRented) {
         this.numHousesRented = numHousesRented;
         this.numHousesListed = numHousesListed;
         this.numActiveListings = numActiveListings;
@@ -28,9 +27,9 @@ public class Report {
             output.write("Houses Listed: "+numHousesListed+"\n");
             output.write("Houses Active: "+numActiveListings+"\n");
             output.write("Houses Rented:\n\n");
-            for(int i=0;i<(int)housesRented.size();i++){
+            for(int i=0;i<(int)housesRented.length;i++){
                 //loop through each property that is rented in DB
-                //output.write(housesRented.get(i).getId()+"   "+housesRented.get(i).getType()+"   "+housesRented.get(i).getAddress()+"   "+housesRented.get(i).getOwner().name);
+                output.write(i+".  Landlord's Name: "+housesRented[i][0]+"   Property Id: "+housesRented[i][1]+"  Property address: "+housesRented[i][2]);
                 output.write("\n");
             }
             output.close();
