c3f3b42c8420b287c44741a2ffcf8887661dda1c
Ahmed Waly <67707288+ahmedw14@users.noreply.github.com>
2021-12-08 18:17:54 -0700

    Update RegisteredRenter.java

--- GroupProject/src/model/RegisteredRenter.java
+++ GroupProject/src/model/RegisteredRenter.java
@@ -1,20 +1,36 @@
+/**
+ * Rental Property Management System Group 26
+ * @author Ahmed Waly
+ * @author Kai Wang
+ * @author Jaxson Waterstreet
+ * @author Dylan Windsor
+ * @author Mohamed Yassin
+ * @version     1.1
+ * @since       1.0
+ */
 package model;
 
 public class RegisteredRenter extends Renter{
-    
+    /**
+     * This constructor initializes the Registered Renter's Credentials, which will be 
+     * stored in the User class(parent class)
+     * @param email String of the Registered Renter's email
+     * @param name String of the Registered Renter's name
+     * @param pass String of the Registered Renter's password
+     */
     public RegisteredRenter(String email, String name, String pass){
         super(email, name, pass);
     }
+    
+    /**
+     * his constructor initializes the Registered Renter's Credentials, which will be 
+     * stored in the User class(parent class) using the parent renter class
+     * @param r The parent Renter object
+     */
     public RegisteredRenter(Renter r){
         super(r.email, r.name, r.password);
     }
-    /*
-    public void saveSearchCriteria(PropertyType type, int numBedrooms, int numBathrooms, boolean furnished, CityQuadrant quadrant){
-        //save this criteria to the db
-    }*/
-    public void unsubscribe(){
-        //delete the object
-    }
+    
     
     
 }
