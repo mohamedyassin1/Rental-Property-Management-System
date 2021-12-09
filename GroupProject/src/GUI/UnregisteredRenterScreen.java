124e3339ef7e0e1e6a90a74a8d87d8c26b4878db
Mohamed Yassin <73013959+mohamedyassin1@users.noreply.github.com>
2021-12-09 03:51:35 -0700

    Documented Unregistered Renter Screen.

--- GroupProject/src/GUI/UnregisteredRenterScreen.java
+++ GroupProject/src/GUI/UnregisteredRenterScreen.java
@@ -1,3 +1,13 @@
+/**
+ * Rental Property Management System Group 26
+ * @author Ahmed Waly
+ * @author Kai Wang
+ * @author Jaxson Waterstreet
+ * @author Dylan Windsor
+ * @author Mohamed Yassin
+ * @version     2.4
+ * @since       1.0
+ */
 package GUI;
 
 import java.awt.Color;
@@ -20,16 +30,30 @@ import javax.swing.SpringLayout;
 import javax.swing.table.DefaultTableModel;
 
 import controller.*;
-
+/**
+ * This screen is for guest users to check out the listings without logging in.
+ * It is convenient because you can just view the listings, and if there is something interesting,
+ * then user can make an account and email landlord, or wait for new properties with the notification setting.
+ *
+ */
 public class UnregisteredRenterScreen implements Component{
+	/*
+	 * Default constructor, calls draw method.
+	 */
 	UnregisteredRenterScreen() {
 		draw();
 	}
-
+	/*
+	 * Main draw method.
+	 * Draws all the GUI components.
+	 * Has functionality inside its buttons action performed methods.
+	 */
 	@SuppressWarnings("serial")
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
+		/*
+		 * Frame setup, clearing screen, setting size and location, layout, background color, etc.
+		 */
 		frame.setTitle("Guest login");
 		frame.setResizable(false);
 		frame.getContentPane().removeAll();
@@ -43,11 +67,17 @@ public class UnregisteredRenterScreen implements Component{
 		frame.getContentPane().setForeground(new Color(255, 255, 255));
 		frame.getContentPane().setBackground(new Color(255, 255, 255));
 		frame.getContentPane().setLayout(null);
-		
+		/*
+		 * ScrollPane so that our tables can be scrolled through for viewing convenience.
+		 */
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 238, 466, 215);
 		frame.getContentPane().add(scrollPane);
-		ViewSearchCriteria v = new ViewSearchCriteria();
+		ViewSearchCriteria v = new ViewSearchCriteria(); //ViewSearchCriteria controller to help get properties.
+		/*
+		 * Table to display properties listed.
+		 * Has 6 columns: PropertyID, PropertyType, Number of Bedrooms, Number of Bathrooms, Furnished, and Quadrant.
+		 */
 		JTable table = new JTable();
 		table.setBackground(new Color(255, 255, 255));
 		table.setModel(new DefaultTableModel(
@@ -71,19 +101,25 @@ public class UnregisteredRenterScreen implements Component{
 		table.getColumnModel().getColumn(5).setPreferredWidth(52);
 		scrollPane.setViewportView(table);
 		
-		
+		/*
+		 * Decorative Panel for the title.
+		 */
 		JPanel titlePanel = new JPanel();
 		titlePanel.setBackground(new Color(0, 191, 255));
 		titlePanel.setBounds(0, 0, 486, 82);
 		frame.getContentPane().add(titlePanel);
 		titlePanel.setLayout(null);
-		
+		/*
+		 * Title Label for the panel.
+		 */
 		JLabel Title = new JLabel("Browse Properties");
 		Title.setBounds(135, 20, 220, 32);
 		Title.setForeground(new Color(255, 255, 255));
 		titlePanel.add(Title);
 		Title.setFont(new Font("SansSerif", Font.BOLD, 24));
-		
+		/*
+		 * Combo Box to select preferred property type.
+		 */
 		JComboBox PropertyType = new JComboBox();
 		PropertyType.setModel(new DefaultComboBoxModel(new String[] {"APARTMENT", "TOWNHOUSE", "DUPLEX", "CONDO", "STUDIO", "BASEMENT"}));
 		PropertyType.setBackground(new Color(0, 191, 255));
@@ -91,7 +127,9 @@ public class UnregisteredRenterScreen implements Component{
 		PropertyType.setBounds(10, 192, 73, 21);
 		PropertyType.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(PropertyType);
-		
+		/*
+		 * Combo Box to select preferred number of bedrooms.
+		 */
 		JComboBox NumBedrooms = new JComboBox();
 		NumBedrooms.setModel(new DefaultComboBoxModel(new Integer[] {1,2,3,4,5,7,8}));
 		NumBedrooms.setFont(new Font("SansSerif", Font.PLAIN, 12));
@@ -99,7 +137,9 @@ public class UnregisteredRenterScreen implements Component{
 		NumBedrooms.setBounds(108, 192, 67, 21);
 		NumBedrooms.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(NumBedrooms);
-		
+		/*
+		 * Combo Box to select preferred number of bathrooms.
+		 */
 		JComboBox NumBathrooms = new JComboBox();
 		NumBathrooms.setModel(new DefaultComboBoxModel(new Integer[] {1,2,3,4,5,6,7,8}));
 		NumBathrooms.setFont(new Font("SansSerif", Font.PLAIN, 12));
@@ -107,7 +147,9 @@ public class UnregisteredRenterScreen implements Component{
 		NumBathrooms.setBounds(196, 192, 60, 21);
 		NumBathrooms.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(NumBathrooms);
-		
+		/*
+		 * Combo Box to select preferred quadrant.
+		 */
 		JComboBox Quadrant = new JComboBox();
 		Quadrant.setModel(new DefaultComboBoxModel(new String[] {"NW", "SW", "NE", "SE"}));
 		Quadrant.setFont(new Font("SansSerif", Font.PLAIN, 12));
@@ -115,33 +157,49 @@ public class UnregisteredRenterScreen implements Component{
 		Quadrant.setBounds(374, 193, 88, 21);
 		Quadrant.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(Quadrant);
-		
+		/*
+		 * Radio Button to select if user prefers furnished or unfurnished.
+		 */
 		JRadioButton Furnished = new JRadioButton("Furnished");
 		Furnished.setBounds(280, 193, 88, 21);
 		Furnished.setBackground(new Color(0, 191, 255));
 		Furnished.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(Furnished);
-		
+		/*
+		 * Label to indicate that is where user selects preferred property type.
+		 */
 		JLabel label1 = new JLabel("PropertyType");
 		label1.setBounds(10, 168, 79, 13);
 		frame.getContentPane().add(label1);
-		
+		/*
+		 * Label to indicate that is where user selects preferred Number of Bedrooms.
+		 */
 		JLabel label2 = new JLabel("#Bedrooms");
 		label2.setBounds(108, 168, 67, 13);
 		frame.getContentPane().add(label2);
-		
+		/*
+		 * Label to indicate that is where user selects preferred Number of Bathrooms.
+		 */
 		JLabel label3 = new JLabel("#Bathrooms");
 		label3.setBounds(200, 168, 73, 13);
 		frame.getContentPane().add(label3);
-		
+		/*
+		 * Label to indicate that this is where user selects if you want furnished or not.
+		 */
 		JLabel label4 = new JLabel("Furnished?");
 		label4.setBounds(293, 168, 67, 13);
 		frame.getContentPane().add(label4);
-		
+		/*
+		 * Label to indicate that this is where user selects preferred quadrant.
+		 */
 		JLabel label5 = new JLabel("Quadrant");
 		label5.setBounds(388, 168, 88, 13);
 		frame.getContentPane().add(label5);
-		
+		/*
+		 * Button to search based on user input in combo boxes and radio buttons.
+		 * Uses ViewSearchCriteria controller to get a new table with accurate results.
+		 * Has an actionPerformed method which activates functionality when button is clicked.
+		 */
 		JButton Search = new JButton("Search");
 		Search.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -184,7 +242,10 @@ public class UnregisteredRenterScreen implements Component{
 		Search.setBounds(135, 92, 198, 52);
 		Search.setForeground(new Color(255, 255, 255));
 		frame.getContentPane().add(Search);
-		
+		/*
+		 * Button to return to main menu
+		 * When actionPerformed is triggered through a user event, goes back to the main menu.
+		 */
 		JButton returnButton = new JButton("Main Menu");
 		returnButton.setFont(new Font("SansSerif", Font.BOLD, 10));
 		returnButton.setBackground(new Color(0, 191, 255));
