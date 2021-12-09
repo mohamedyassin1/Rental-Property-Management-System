153059c86b62248e961e41436c72553f056bdae7
Ahmed Waly <67707288+ahmedw14@users.noreply.github.com>
2021-12-08 18:17:28 -0700

    Update Renter.java

--- GroupProject/src/model/Renter.java
+++ GroupProject/src/model/Renter.java
@@ -1,24 +1,35 @@
+/**
+ * Rental Property Management System Group 26
+ * @author Ahmed Waly
+ * @author Kai Wang
+ * @author Jaxson Waterstreet
+ * @author Dylan Windsor
+ * @author Mohamed Yassin
+ * @version     1.2
+ * @since       1.0
+ */
 package model;
-
+/**
+ * This Renter class is a child of the user, and there can be two types of 
+ * Renters (Registered and Unregistered). The registered renter will call the 
+ * default constructor and the registered will need to pass in their email, username, and password.
+ */
 public class Renter extends User{
+    /**
+     * Constructor that will pass to the User class, the renter's email, name, and 
+     * password
+     * @param email Renter's Email
+     * @param name  Renter's Name
+     * @param password  Renter's password
+     */
     public Renter(String email, String name,String password){
         super(email, name, password);
     }
+    /**
+     * Constructor for the unregistered Renter where they don't have a name, email, or password
+     */
     public Renter(){
         super(); //unregistered
     }
     
-    public void needFurtherDetails(String message){
-        //send message to landlord(anonymous)
-    }
-    public void arrangeNeeting(String message){
-        //send message to landlord(knowing their name and email)
-    }
-    public void SearchProperties(){
-        //get All Properties in DB
-    }
-    /*
-    public void SearchPropertiesByCriteria(PropertyType type, int numBedrooms, int numBathrooms, boolean furnished, CityQuadrant quadrant){
-        //get all properties that match the criteria
-    }*/
 }
