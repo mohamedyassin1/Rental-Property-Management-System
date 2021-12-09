fe7023a90c81b682d90f1b58b2a1e55231b91645
Ahmed Waly <67707288+ahmedw14@users.noreply.github.com>
2021-12-08 18:18:32 -0700

    Update Property.java

--- GroupProject/src/model/Property.java
+++ GroupProject/src/model/Property.java
@@ -1,25 +1,17 @@
+/**
+ * Rental Property Management System Group 26
+ * @author Ahmed Waly
+ * @author Kai Wang
+ * @author Jaxson Waterstreet
+ * @author Dylan Windsor
+ * @author Mohamed Yassin
+ * @version     3.4
+ * @since       1.0
+ */
 package model;
-/*
-enum PropertyType {
-    ATTACHEDHOUSE,
-    DETACHEDHOUSE,
-    APARTMENT,
-    TOWNHOUSE,
-    CONDO
-}
-public enum PropertyStatus {
-    ACTIVE,
-    RENTED,
-    CANCELLED,
-    SUSPENDED
-}
-public enum CityQuadrant {
-    NW,
-    NE,
-    SW,
-    SE
-}*/
-
+/**
+ * This property class stores all the required information of any property
+ */
 public class Property {
     
     private String propertyStatus;
@@ -31,6 +23,18 @@ public class Property {
     private String address;
     private int id;
     private String email;
+    /**
+     * This constructor initializes the property information
+     * @param propertyStatus String of the property status
+     * @param propertyType String of the property type
+     * @param numBedrooms integer of the number of bedrooms the property has
+     * @param numBathrooms integer of the number of bathrooms the property has
+     * @param furnished boolean if the property is furnished(true) or not(false)
+     * @param quadrant String of the Quadrant where the property is stored
+     * @param address String of the address
+     * @param id integer of the id
+     * @param loggedinEmail String of the owner's email
+     */
     public Property(String propertyStatus, String propertyType, int numBedrooms, int numBathrooms, boolean furnished,
             String quadrant, String address, int id, String loggedinEmail) {
         this.propertyStatus = propertyStatus;
@@ -43,42 +47,81 @@ public class Property {
         this.id = id;
         this.email = loggedinEmail;
     }
+    /**
+     * Getter for the status of a property
+     * @return String of the property status
+     */
     public String getStatus() {
         return propertyStatus;
     }
+    
+    /**
+     * Setter for the status of the property
+     * @param propertyStatus String of the property status
+     */
     public void setStatus(String propertyStatus) {
         this.propertyStatus = propertyStatus;
     }
+    /**
+     * Getter for the type of the property
+     * @return String for the type of the property
+     */
     public String getType() {
         return propertyType;
     }
-    
+    /**
+     * Getter for the number of the bedrooms
+     * @return integer for the number of bedrooms
+     */
     public int getNumBedrooms() {
         return numBedrooms;
     }
-    
+    /**
+     * Getter for the number of the bathrooms
+     * @return integer for the number of bedrooms
+     */
     public int getNumBathrooms() {
         return numBathrooms;
     }
-    
+    /**
+     * Getter of the boolean if the property is furnished
+     * @return boolean if the property is furnished(true) or not(false)
+     */
     public boolean isFurnished() {
         return furnished;
     }
+    /**
+     * Setter of the boolean if the property is furnished
+     * @param furnished boolean if the property is furnished(true) or not(false)
+     */
     public void setFurnished(boolean furnished) {
         this.furnished = furnished;
     }
+    /**
+     * Getter for the quadrant of which the property is located in the city
+     * @return String of the Quadrant where the property is stored
+     */
     public String getQuadrant() {
         return quadrant;
     }
-    
+    /**
+     * Getter for the property's Address
+     * @return String of the address
+     */
     public String getAddress() {
         return address;
     }
-    
+    /**
+     * Getter for the property id
+     * @return integer of the id
+     */
     public int getId() {
         return id;
     }
-    
+    /**
+     * getter for the email address of the property's owner
+     * @return String of the email
+     */
     public String getEmail() {
         return email;
     }
